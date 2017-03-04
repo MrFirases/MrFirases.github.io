41f575f5a36077c49217831a1c7397872fcd4d38
MrFirases <mohamedfiras.ouertani@esprit.tn>
2017-03-04 20:20:56 +0100

    Editing mailing function

--- scripts/contact.php
+++ scripts/contact.php
@@ -2,13 +2,5 @@
 <?php
 echo 'You are forbidden!';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage'])) {
-
-		$to = 'profirases@gmail.com'; // Replace with your email.
-		$body = "Name: {$_POST['contactname']}\n\nEmail: {$_POST['contactemail']}\n\nMessage: {$_POST['contactmessage']}";
-		mail($to, "Contact Form Submission", $body, "From: {$_POST['contactemail']}");
-	}
-}
 ?>
 

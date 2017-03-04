756b21a73a18b4c3fe4e332c8dda1bc57b20bf49
MrFirases <mohamedfiras.ouertani@esprit.tn>
2017-03-04 20:10:34 +0100

    Editing mailing function

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,28 +1,10 @@
 
 <?php
-
-<?php
-// The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
-
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
-
-// Send
-mail('profirases@gmail.com', 'My Subject', $message);
-?>
-
-
-
-
-
-
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if(!empty($_POST['contact_name']) && !empty($_POST['contact_email']) && !empty($_POST['contact_message'])) {
+	if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage'])) {
 		$to = 'profirases@gmail.com'; // Replace with your email.
-		$body = "Name: {$_POST['contact_name']}\n\nEmail: {$_POST['contact_email']}\n\nMessage: {$_POST['contact_message']}";
-		mail($to, "Contact Form Submission", $body, "From: {$_POST['contact_email']}");
+		$body = "Name: {$_POST['contactname']}\n\nEmail: {$_POST['contactemail']}\n\nMessage: {$_POST['contactmessage']}";
+		mail($to, "Contact Form Submission", $body, "From: {$_POST['contactemail']}");
 	}
 }
 ?>

474f3beb91f3be72b33747a354067224c4509602
MrFirases <mohamedfiras.ouertani@esprit.tn>
2017-03-04 20:06:36 +0100

    Editing mailing function

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,13 +1,29 @@
 
-
+<?php
 
 <?php
-$to      = 'profirases@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: mohamedfiras.ouertani@esprit.tn' . "\r\n" .
-    'Reply-To: mohamedfiras.ouertani@esprit.tn' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+// The message
+$message = "Line 1\r\nLine 2\r\nLine 3";
 
-mail($to, $subject, $message, $headers);
+// In case any of our lines are larger than 70 characters, we should use wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+// Send
+mail('profirases@gmail.com', 'My Subject', $message);
 ?>
+
+
+
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if(!empty($_POST['contact_name']) && !empty($_POST['contact_email']) && !empty($_POST['contact_message'])) {
+		$to = 'profirases@gmail.com'; // Replace with your email.
+		$body = "Name: {$_POST['contact_name']}\n\nEmail: {$_POST['contact_email']}\n\nMessage: {$_POST['contact_message']}";
+		mail($to, "Contact Form Submission", $body, "From: {$_POST['contact_email']}");
+	}
+}
+?>
+
